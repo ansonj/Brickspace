b61b8d64c607757e1372a72fa415e00fb2ef9b3f
Anson Jablinski <a@ansonj.org>
2014-06-06 17:15:28 -0500

    Finished up Flat Pyramid!

--- Brickspace/Brick-related model classes/GenericDesign stuff/Designs themselves/BKP_GD_FlatPyramid.m
+++ Brickspace/Brick-related model classes/GenericDesign stuff/Designs themselves/BKP_GD_FlatPyramid.m
@@ -14,33 +14,87 @@
 	return YES;
 }
 
-+ (NSString *)name {
++ (NSString *)designName {
 	return @"Flat Pyramid";
 }
 
-+ (BOOL)canBeBuiltFromBrickSet:(BKPBrickSet *)inputBricks {
-	return [inputBricks brickCount] > 0;
++ (NSString *)designDescription {
+	return @"A triangular pyramid, only one brick deep, of full-height 2x4s.";
 }
 
++ (NSString *)description {
+	return [NSString stringWithFormat:@"GenericDesign %@: %@", [self designName], [self designDescription]];
+}
 
++ (BOOL)canBeBuiltFromBrickSet:(BKPBrickSet *)inputBricks {
+	return [[self bricksToBeUsedInModelFromSet:inputBricks] brickCount] > 0;
+}
 
++ (BKPRealizedModel *)createRealizedModelUsingBrickSet:(BKPBrickSet *)inputBricks {
+	BKPRealizedModel *model = [[BKPRealizedModel alloc] initWithSourceDesignName:[self designName]];
+	
+	BKPBrickSet *bricksToUse = [self bricksToBeUsedInModelFromSet:inputBricks];
+	
+	// assign bricks (you have the perfect number) to the realized model
+	int bricksUsedSoFar = 0;
+	int totalBricksToUse = [bricksToUse brickCount];
+	int currentX = 3.0 - sqrtf(1 + 8 * totalBricksToUse);
+	int currentDirection = 1;
+	int currentZ = 0;
+	int currentRowCount = 0;
+	int targetRowCount = (-1.0 + sqrtf(1 + 8 * totalBricksToUse)) / 2.0;
+	while (bricksUsedSoFar < totalBricksToUse) {
+		BKPBrick *currentBrick = [[bricksToUse setOfBricks] anyObject];
+		
+		BKPPlacedBrick *placedBrick = [[BKPPlacedBrick alloc] init];
+		placedBrick.brick = currentBrick;
+		placedBrick.orientation = BKPPlacedBrickOrientationAlongXAxis;
+		[placedBrick setX:currentX Y:0 andZ:currentZ];
+		[model addPlacedBrick:placedBrick];
+		
+		[bricksToUse removeBrick:currentBrick];
+		
+		bricksUsedSoFar++;
+		
+		currentRowCount++;
+		if (currentRowCount == targetRowCount) {
+			currentDirection *= -1;
+			currentX += currentDirection * 2;
+			currentZ++;
+			currentRowCount = 0;
+			targetRowCount--;
+		} else {
+			currentX += currentDirection * 4;
+		}
+		
+	}
+	
+	return model;
+}
 
 + (float)percentUtilizedIfBuiltWithSet:(BKPBrickSet *)inputBricks {
 	assert([self canBeBuiltFromBrickSet:inputBricks]);
 	
-	int brickCount = (int)[inputBricks brickCount];
-	
-	return 100.0 * [self numberOfBricksUsedInPyramidIfGiven:brickCount] / brickCount;
+	return 100.0 * [[self bricksToBeUsedInModelFromSet:inputBricks] brickCount] / [inputBricks brickCount];
 }
 
-
-// helpers
-
-+ (float)numberOfBricksUsedInPyramidIfGiven:(int)bricks {
-	// https://en.wikipedia.org/wiki/Triangular_number
-	int triangularIndex = (int)((-1.0 + sqrtf(1 + 8 * bricks)) / 2.0);
++ (BKPBrickSet *)bricksToBeUsedInModelFromSet:(BKPBrickSet *)inputBricks {
+	BKPBrickSet *bricksToBeUsed = [BKPBrickSet set];
+		
+	for (BKPBrick *brick in [inputBricks setOfBricks]) {
+		if ([brick height] == BKPBrickHeightFull && [brick size] == BKPBrickSize2x4) {
+			[bricksToBeUsed addBrick:brick];
+		}
+	}
 	
-	return triangularIndex * (triangularIndex + 1) / 2.0;
+	int triangularIndex = (int)((-1.0 + sqrtf(1 + 8 * [bricksToBeUsed brickCount])) / 2.0);
+	int numberOfBricksInPyramid = triangularIndex * (triangularIndex + 1) / 2.0;
+	
+	while ([bricksToBeUsed brickCount] > numberOfBricksInPyramid) {
+		[bricksToBeUsed removeBrick:[[bricksToBeUsed setOfBricks] anyObject]];
+	}
+	
+	return bricksToBeUsed;
 }
 
 @end
