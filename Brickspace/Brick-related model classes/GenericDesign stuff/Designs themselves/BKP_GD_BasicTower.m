2506ae90034abf7ca314ad7d332f1a531113c91e
Anson Jablinski <a@ansonj.org>
2014-06-06 16:04:38 -0500

    Finished up BasicTower.

--- Brickspace/Brick-related model classes/GenericDesign stuff/Designs themselves/BKP_GD_BasicTower.m
+++ Brickspace/Brick-related model classes/GenericDesign stuff/Designs themselves/BKP_GD_BasicTower.m
@@ -14,19 +14,52 @@
 	return YES;
 }
 
-+ (NSString *)name {
++ (NSString *)designName {
 	return @"Basic Tower";
 }
 
-+ (BOOL)canBeBuiltFromBrickSet:(BKPBrickSet *)inputBricks {
-	return [inputBricks brickCount] >= 2;
++ (NSString *)designDescription {
+	return @"A tower of full-height 2x4s stacked atop one another.";
 }
 
++ (BOOL)canBeBuiltFromBrickSet:(BKPBrickSet *)inputBricks {
+	return [[self bricksToBeUsedInModelFromSet:inputBricks] brickCount] >= 2;
+}
+
++ (BKPRealizedModel *)createRealizedModelUsingBrickSet:(BKPBrickSet *)inputBricks {
+	BKPRealizedModel *model = [[BKPRealizedModel alloc] initWithSourceDesignName:self.designName];
+	
+	BKPBrickSet *bricksToUse = [self bricksToBeUsedInModelFromSet:inputBricks];
+	
+	float zCoord = 0;
+	for (BKPBrick *brick in [bricksToUse setOfBricks]) {
+		BKPPlacedBrick *placedBrick = [[BKPPlacedBrick alloc] init];
+		placedBrick.brick = brick;
+		placedBrick.orientation = BKPPlacedBrickOrientationAlongXAxis;
+		[placedBrick setX:0 Y:0 andZ:zCoord];
+		zCoord++;
+		[model addPlacedBrick:placedBrick];
+	}
+	
+	return model;
+}
 
 + (float)percentUtilizedIfBuiltWithSet:(BKPBrickSet *)inputBricks {
 	assert([self canBeBuiltFromBrickSet:inputBricks]);
 	
 	return 100;
+}
+
++ (BKPBrickSet *)bricksToBeUsedInModelFromSet:(BKPBrickSet *)inputBricks {
+	BKPBrickSet *bricksToBeUsed = [BKPBrickSet set];
+	
+	for (BKPBrick *brick in [inputBricks setOfBricks]) {
+		if ([brick height] == BKPBrickHeightFull && [brick size] == BKPBrickSize2x4) {
+			[bricksToBeUsed addBrick:brick];
+		}
+	}
+	
+	return bricksToBeUsed;
 }
 
 @end
