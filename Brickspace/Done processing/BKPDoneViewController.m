a03b21799e631de92c957306620e0d73e77422fe
Anson Jablinski <a@ansonj.org>
2014-06-11 16:49:53 -0500

    Finished up done / instruction view, for now

--- Brickspace/Done processing/BKPDoneViewController.m
+++ Brickspace/Done processing/BKPDoneViewController.m
@@ -8,46 +8,106 @@
 
 #import "BKPDoneViewController.h"
 
+#import "BKPLegoView.h"
 #import "BKP_GDManager.h"
 #import "BKPGenericDesign.h"
+#import "BKPInstructionSet.h"
 
 @interface BKPDoneViewController ()
 @property (weak, nonatomic) IBOutlet UITextView *summaryTextView;
+@property (nonatomic) NSString *summaryText;
+@property (weak, nonatomic) IBOutlet BKPLegoView *legoView;
+@property (weak, nonatomic) IBOutlet UISlider *stepSlider;
+@property (weak, nonatomic) IBOutlet UILabel *stepLabel;
+
+@property (nonatomic) NSSet *countedBrickSet;
+@property (nonatomic) id<BKPGenericDesign> selectedDesign;
+@property (nonatomic) BKPInstructionSet *instructions;
+@property (nonatomic) int currentStepNumber;
 @end
 
-@implementation BKPDoneViewController {
-	NSSet *completedBrickSet;
-}
+@implementation BKPDoneViewController
 
-@synthesize summaryTextView;
+@synthesize summaryTextView, summaryText;
+@synthesize legoView;
+@synthesize stepSlider, stepLabel;
+
+@synthesize countedBrickSet;
+@synthesize selectedDesign;
+@synthesize instructions;
+@synthesize currentStepNumber;
 
 - (void)setUpWithCountedBricks:(NSSet *)newSet {
-	completedBrickSet = newSet;
+	countedBrickSet = newSet;
 	
-	[self updateUI];
+	// figure out which is the best
+	summaryText = [NSString stringWithFormat:@"A BKPBrickSet was created with %lu bricks in it.\n\n",(unsigned long)[countedBrickSet count]];
+	
+	NSArray *availableDesigns = [BKP_GDManager availableDesigns];
+	for (id<BKPGenericDesign> design in availableDesigns) {
+		if ([design canBeBuiltFromBricks:countedBrickSet]) {
+			selectedDesign = design;
+			NSString *designName = [design designName];
+			float percentage = [design percentUtilizedIfBuiltWithSet:countedBrickSet];
+			summaryText = [summaryText stringByAppendingFormat:@"You can build a %@ with %.1f%% brick utilization!\n", designName, percentage];
+		}
+	}
+	
+	summaryText = [summaryText stringByAppendingFormat:@"\n\nToday, you'll be building a %@, which is a %@.", [selectedDesign designName], [selectedDesign designDescription]];
+	
+	BKPRealizedModel *model = [selectedDesign createRealizedModelUsingBricks:countedBrickSet];
+	instructions = [BKPInstructionGenerator instructionsForRealizedModel:model withStyle:BKPInstructionGeneratorStyleBottomUp];
+	
+	currentStepNumber = 1;
+	
+	[legoView setDrawAxes:NO];
+	[legoView setDrawBaseplate:YES];
+	[legoView setBaseplateColor:BKPBrickColorBlue andSize:8];
+	
+	// setup should be called first, before it appears, so it should be okay to comment this out
+//	[self initializeUI];
 }
 
 - (void)viewDidAppear:(BOOL)animated {
+	[self initializeUI];
+}
+
+- (void)initializeUI {
+	[summaryTextView setText:summaryText];
+	
+	[stepSlider setMinimumValue:1];
+	[stepSlider setMaximumValue:[instructions stepCount]];
+	[stepSlider setValue:1 animated:NO];
+	
 	[self updateUI];
 }
 
 - (void)updateUI {
-	NSString *summary = [NSString stringWithFormat:@"A BKPBrickSet was created with %lu bricks in it.\n\n",(unsigned long)[completedBrickSet count]];
-	
-	id<BKPGenericDesign> bestDesign;
-	NSArray *availableDesigns = [BKP_GDManager availableDesigns];
-	for (id<BKPGenericDesign> design in availableDesigns) {
-		if ([design canBeBuiltFromBricks:completedBrickSet]) {
-			bestDesign = design;
-			NSString *designName = [design designName];
-			float percentage = [design percentUtilizedIfBuiltWithSet:completedBrickSet];
-			summary = [summary stringByAppendingFormat:@"You can build a %@ with %.1f%% brick utilization!\n", designName, percentage];
-		}
-	}
-	
-	summary = [summary stringByAppendingFormat:@"\n\nToday, you'll be building a %@, which is a %@.", [bestDesign designName], [bestDesign designDescription]];
-	
-	[summaryTextView setText:summary];
+	// update slider position
+	[stepSlider setValue:currentStepNumber animated:YES];
+	// update label text
+	[stepLabel setText:[NSString stringWithFormat:@"%d of %d", currentStepNumber, [instructions stepCount]]];
+	// tell legoview the right bricks to draw
+	[legoView displayBricks:[instructions bricksForStepsOneThrough:currentStepNumber]];
+}
+
+- (IBAction)sliderMoved:(UISlider *)sender {
+	currentStepNumber = (int)[sender value];
+	[self updateUI];
+}
+
+- (IBAction)backStepPressed:(UIButton *)sender {
+	currentStepNumber--;
+	if (currentStepNumber <= 0)
+		currentStepNumber = 1;
+	[self updateUI];
+}
+
+- (IBAction)forwardStepPressed:(UIButton *)sender {
+	currentStepNumber++;
+	if (currentStepNumber > [instructions stepCount])
+		currentStepNumber = [instructions stepCount];
+	[self updateUI];
 }
 
 - (void)didReceiveMemoryWarning
