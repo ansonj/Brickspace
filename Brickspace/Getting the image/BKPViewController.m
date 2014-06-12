5b403ca24a0842b6b48d2c3056f4db0e4f2d4abf
Anson Jablinski <a@ansonj.org>
2014-06-12 13:38:07 -0500

    Added some rendered bricks to the “title” screen

--- Brickspace/Getting the image/BKPViewController.m
+++ Brickspace/Getting the image/BKPViewController.m
@@ -8,22 +8,33 @@
 
 #import "BKPViewController.h"
 
-@interface BKPViewController ()
+#import "BKPLegoView.h"
+#import "BKPPlacedBrick.h"
 
+@interface BKPViewController ()
+@property (weak, nonatomic) IBOutlet BKPLegoView *legoView;
 @end
 
 @implementation BKPViewController
 
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
-}
+@synthesize legoView;
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)viewDidAppear:(BOOL)animated {
+	NSMutableArray *brickSet = [NSMutableArray array];
+	BKPBrick *oneBrickToRuleThemAll = [BKPBrick brickWithColor:BKPBrickColorRed shortSide:2 longSide:4 andHeight:3];
+	for (int count = 0; count < 6; count++) {
+		BKPPlacedBrick *placedBrick = [[BKPPlacedBrick alloc] init];
+		[placedBrick setBrick:oneBrickToRuleThemAll];
+		[brickSet addObject:placedBrick];
+	}
+	[brickSet[0] setX:-4 Y:0 andZ:0];
+	[brickSet[1] setX:0 Y:0 andZ:0];
+	[brickSet[2] setX:4 Y:0 andZ:0];
+	[brickSet[3] setX:-2 Y:0 andZ:3];
+	[brickSet[4] setX:2 Y:0 andZ:3];
+	[brickSet[5] setX:0 Y:0 andZ:6];
+	
+	[legoView displayBricks:[NSSet setWithArray:brickSet]];
 }
 
 @end
