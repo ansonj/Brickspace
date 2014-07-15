e6df8992ca7f7ea287939fc1ffaab7780a4c6dcd
Anson Jablinski <a@ansonj.org>
2014-07-15 17:13:06 -0500

    Added start button connection, made splash bricks have random colors, and lined up coordinates.

--- Brickspace/00 Launch and splash/BKPSplashViewController.m
+++ Brickspace/00 Launch and splash/BKPSplashViewController.m
@@ -8,8 +8,12 @@
 
 #import "BKPSplashViewController.h"
 
+// For random bricks inside the little LegoView
 #import "BKPLegoView.h"
 #import "BKPPlacedBrick.h"
+
+// For loading the next VC
+#import "BKPCapturingViewController.h"
 
 @interface BKPSplashViewController ()
 @property (weak, nonatomic) IBOutlet BKPLegoView *legoView;
@@ -21,20 +25,24 @@
 
 - (void)viewDidAppear:(BOOL)animated {
 	NSMutableArray *brickSet = [NSMutableArray array];
-	BKPBrick *oneBrickToRuleThemAll = [BKPBrick brickWithColor:BKPBrickColorRed shortSide:2 longSide:4 andHeight:3];
 	for (int count = 0; count < 6; count++) {
 		BKPPlacedBrick *placedBrick = [[BKPPlacedBrick alloc] init];
-		[placedBrick setBrick:oneBrickToRuleThemAll];
+		[placedBrick setBrick:[BKPBrick brickWithColor:[BKPBrickColorOptions randomColor] shortSide:2 longSide:4 andHeight:3]];
 		[brickSet addObject:placedBrick];
 	}
 	[brickSet[0] setX:-4 Y:0 andZ:0];
-	[brickSet[1] setX:0 Y:0 andZ:0];
-	[brickSet[2] setX:4 Y:0 andZ:0];
+	[brickSet[1] setX:0  Y:0 andZ:0];
+	[brickSet[2] setX:4  Y:0 andZ:0];
 	[brickSet[3] setX:-2 Y:0 andZ:3];
-	[brickSet[4] setX:2 Y:0 andZ:3];
-	[brickSet[5] setX:0 Y:0 andZ:6];
+	[brickSet[4] setX:2  Y:0 andZ:3];
+	[brickSet[5] setX:0  Y:0 andZ:6];
 	
 	[legoView displayBricks:[NSSet setWithArray:brickSet]];
+}
+
+- (IBAction)startButtonPressed:(id)sender {
+	BKPCapturingViewController *capturingVC = [[BKPCapturingViewController alloc] init];
+	[[[UIApplication sharedApplication] keyWindow] setRootViewController:capturingVC];
 }
 
 @end
