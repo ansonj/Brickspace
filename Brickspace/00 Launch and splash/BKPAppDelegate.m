de480508570054bdf917cdba799b0db887a1de35
Anson Jablinski <a@ansonj.org>
2014-09-14 17:29:33 -0500

    No intro logging statement, please

--- Brickspace/00 Launch and splash/BKPAppDelegate.m
+++ Brickspace/00 Launch and splash/BKPAppDelegate.m
@@ -28,7 +28,7 @@ static int loggingPort = 4999;
 	[[self window] setRootViewController:splashVC];
     
 	[self startWirelessLogging];
-	NSLog(@"\n\n\nGood morning.\nThese are the captain's logs from run starting at %@.", [NSDate date]);
+//	NSLog(@"\n\n\nGood morning.\nThese are the captain's logs from run starting at %@.", [NSDate date]);
 	
 	self.window.backgroundColor = [UIColor whiteColor];
 	[self.window makeKeyAndVisible];
